710bd7c2bcd6fd3396fdac81575bbfff5e484643
Riley Grigg <griggriley@gmail.com>
2026-06-14 17:11:19 -0600

    Revert "Starting widget implementation"
    This reverts commit 1197aab1fae7e7561c5bc1c534af19d2723a9332.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,8 +89,6 @@ dependencies {
     implementation(libs.youtube.player)
     implementation(libs.play.services.location)
     implementation(libs.retrofit)
-    implementation(libs.androidx.glance.appwidget)
-    implementation(libs.androidx.glance.material3)
     testImplementation(libs.androidx.core)
     testImplementation(libs.androidx.junit)
     testImplementation(libs.junit)
