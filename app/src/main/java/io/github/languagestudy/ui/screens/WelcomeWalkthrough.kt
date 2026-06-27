3b7e13522ed34103750481f0afc20b04210c72ae
Riley Grigg <griggriley@gmail.com>
2026-06-27 11:41:34 -0600

    Revert "Settings update: App details updated"
    This reverts commit 4a0c93ade6bdab14d00dd04fa086211315fc900f.

--- app/src/main/java/io/github/languagestudy/ui/screens/WelcomeWalkthrough.kt
+++ app/src/main/java/io/github/languagestudy/ui/screens/WelcomeWalkthrough.kt
@@ -25,6 +25,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.HorizontalDivider
@@ -32,7 +33,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -289,7 +289,7 @@ private fun SelectLanguageStep(
                 readOnly = true,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .menuAnchor(MenuAnchorType.PrimaryNotEditable),
+                    .menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable),
                 trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
                 shape = RoundedCornerShape(12.dp),
                 colors = TextFieldDefaults.colors(
