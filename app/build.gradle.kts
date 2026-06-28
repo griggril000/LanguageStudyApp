e51bce8face7f18200b65dbd352c237076aec077
Riley Grigg <griggriley@gmail.com>
2026-06-27 20:14:42 -0600

    Ready for live commit. Updated version number.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "io.github.languagestudy"
         minSdk = 24
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
