66097d7ebcbbec92704fe1368b58f3921072b3de
Riley Grigg <griggriley@gmail.com>
2026-07-01 15:16:38 -0600

    Merge pull request #20
    * Fixed settings and admin padding; updated depreciated window code; ne…

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "io.github.langstudy"
         minSdk = 24
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.3.1"
+        versionCode = 7
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
