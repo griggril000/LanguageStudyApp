17e7c06385ad7d5227ee6f03d2fccd61da9cf7c9
Riley Grigg <griggriley@gmail.com>
2026-07-12 01:07:19 -0600

    Added app shortcuts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "io.github.langstudy"
         minSdk = 24
         targetSdk = 37
-        versionCode = 18
-        versionName = "1.8.0"
+        versionCode = 19
+        versionName = "1.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
