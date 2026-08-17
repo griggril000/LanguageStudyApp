b059dc813a4d0dd3341b9a9bd2e9e3a09db0696f
Riley Grigg <griggriley@gmail.com>
2026-08-17 17:10:59 -0600

    Bump version to 2.5.0
    * Update `versionCode` to 26 and `versionName` to 2.5.0 in `build.gradle.kts`
    * Update `selectedTabId` to "Android vitals" in `appInsightsSettings.xml`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "io.github.langstudy"
         minSdk = 24
         targetSdk = 37
-        versionCode = 25
-        versionName = "2.4.0"
+        versionCode = 26
+        versionName = "2.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
