559164029b994f8e9934a5f49a75872e63ea5523
Riley Grigg <griggriley@gmail.com>
2026-08-26 18:09:02 -0600

    Remove unused Spacer import in MainActivity.kt

--- app/src/main/java/io/github/langstudy/MainActivity.kt
+++ app/src/main/java/io/github/langstudy/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.consumeWindowInsets
